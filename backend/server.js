a8fd1085aa81c2c5a74c83939cf1466d4de5e937
Sivaji Avulamanda <sivajisalaar@gmail.com>
2025-11-13 11:41:45 +0530

    Update for production: MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error(' MongoDB connection error:', err));
 
